cde7f7ed0c9824a7b4935927775d894fb1c24ac4
Liulele <liulele@qinengda.cn>
2024-04-18 13:44:36 +0800

    线程控制

--- CSharp-_Multithreading-Asynchronous/Program.cs
+++ CSharp-_Multithreading-Asynchronous/Program.cs
@@ -1,11 +1,35 @@
-﻿namespace CSharp__Multithreading_Asynchronous
+﻿
+namespace CSharp__Multithreading_Asynchronous
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+
+
+        //3.线程的控制
+
+        /*static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello LELE!");
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            //为什么下面这种方式是错的，我理解应该是给委托指向的方法传递参数？
+            //Thread thread = new Thread(PrintHello(cancellationTokenSource.Token));
+            Thread thread = new Thread(() => { PrintHello(cancellationTokenSource.Token); });
+            thread.Start();
+
+            //比如我现在有一个下载文件的线程，我只想给5s钟
+            Thread.Sleep(5000);
+            cancellationTokenSource.CancelAfter(1000);
+
+            Console.WriteLine("程序从这里退出！");
         }
+
+        private static void PrintHello(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Hello World!");
+            }
+        }*/
     }
 }
